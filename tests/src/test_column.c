6600964ae4414a8a70269764fa1d24b94dc0a033
johnny <vmjuan90@gmail.com>
2021-05-04 11:23:40 +0200

    add check

--- tests/src/test_column.c
+++ tests/src/test_column.c
@@ -22,16 +22,18 @@ TEST_C(column, create_varchar)
 	strncpy(col->data, "a", (size_t)col->octet_length);
 	struct column *col2 = col->copy(col);
 	CHECK_C(*(char*)col2->data == 'a');
+	CHECK_C(col2->n_constraints == 1);
 	col->free(col);
 	col2->free(col2);
 }
 
 TEST_C(column, create_integer)
 {
-	struct column *col = Column("column", INTEGER(), not_null, COLUMN_END)->property.column;
+	struct column *col = Column("column", INTEGER(), primary_key, not_null, COLUMN_END)->property.column;
 	*(intmax_t*)col->data = INTMAX_MAX;
 	struct column *col2 = col->copy(col);
 	CHECK_C(*(intmax_t*)col2->data == INTMAX_MAX);
+	CHECK_C(col2->n_constraints == 2);
 	col->free(col);
 	col2->free(col2);
 }
