e120c82e2da65951c68f45d7340c3ee91bafd7fc
johnny <vmjuan90@gmail.com>
2021-06-27 10:29:26 +0200

    compile SQL in progress

---
+++ tests/src/test_compile.c
@@ -0,0 +1,36 @@
+#include <string.h>
+
+#include "CppUTest/TestHarness_c.h"
+
+#include "query_builder_error_c.h"
+#include "query_builder_constraint_c.h"
+#include "query_builder_table_c.h"
+#include "query_builder_c.h"
+
+TEST_GROUP_C_SETUP(compile)
+{
+}
+
+TEST_GROUP_C_TEARDOWN(compile)
+{
+}
+
+TEST_C(compile, select)
+{
+	struct query_builder_table* table = Table("table",
+				Column("column", INTEGER(), primary_key),
+				Column("column", VARCHAR(125)),
+				Column("column", VARCHAR(125))
+			);
+
+	struct query_builder* query = Query(NULL);
+	char* query_string = query->compile(Select(table));
+	
+	CHECK_EQUAL_C_STRING_TEXT(
+			query_string,
+			"SELECT table.column,table.column,table.column FROM table",
+			query_string);
+
+	free(query_string);
+	table->free(table);
+}
